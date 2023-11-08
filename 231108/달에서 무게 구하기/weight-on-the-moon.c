d8d720e8c3b2b972a954fa698f33dca58fac8996
이윤형 <139411504+yun73@users.noreply.github.com>
2023-11-08 17:10:21 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231108/달에서 무게 구하기/weight-on-the-moon.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int weight = 13;
+    double ratio = 0.165;
+    printf("%d * %.6f = %.6f", weight,ratio,weight*ratio);
+    return 0;
+}
