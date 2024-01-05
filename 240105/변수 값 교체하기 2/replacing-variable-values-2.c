0b148a4882b8f1eb68286c9ff74911633d8f6b98
이윤형 <139411504+yun73@users.noreply.github.com>
2024-01-05 17:17:30 +0900

    Title: 변수 값 교체하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240105/변수 값 교체하기 2/replacing-variable-values-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 5, b =3;
+    // printf("A is %d\n", a);
+
+    a = b;
+    // printf("A is %d\n",a);
+    printf("%d\n", a);
+    a = 2;
+    printf("%d", b);
+    return 0;
+}
