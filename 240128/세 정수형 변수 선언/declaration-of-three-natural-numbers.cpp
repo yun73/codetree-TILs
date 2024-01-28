b8bdabcbde626cff3ccdccf1d320412f6eceb81d
이윤형 <139411504+yun73@users.noreply.github.com>
2024-01-28 23:25:09 +0900

    Title: 세 정수형 변수 선언, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240128/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 7, b = 23, c= 30;
+
+    cout << a << " + " << b << " = " << c;
+    return 0;
+}
