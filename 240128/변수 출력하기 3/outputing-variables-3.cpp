8c050d0fd22e10224a60d2a94c9d6d3d1b8ebbe2
이윤형 <139411504+yun73@users.noreply.github.com>
2024-01-28 23:29:30 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240128/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a = 1, b= 2;
+    char c = 'C';
+
+    cout << a << "->" << b << "->" << c;
+
+    return 0;
+}
