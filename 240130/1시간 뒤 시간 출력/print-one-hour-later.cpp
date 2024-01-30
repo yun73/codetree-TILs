97567b614c336ac63a30200ddca42dbb79d7f844
이윤형 <139411504+yun73@users.noreply.github.com>
2024-01-30 21:41:36 +0900

    Title: 1시간 뒤 시간 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int h,m;
+    char split;
+
+    cin >> h >> split >> m;
+
+    cout << h + 1 << split << m;
+
+    return 0;
+}
