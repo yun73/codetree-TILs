156cbe0bfc6ee443e7d1dbbfadd07a9f67f107fa
이윤형 <139411504+yun73@users.noreply.github.com>
2024-01-30 21:22:48 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a,b;
+
+    cin >> a >> b;
+
+    cout << fixed;
+
+    cout.precision(2);
+
+    cout << a + b;
+
+    return 0;
+}
