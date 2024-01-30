007ee20950f48fcaaf3d81b172c810c269603a45
이윤형 <139411504+yun73@users.noreply.github.com>
2024-01-30 23:13:50 +0900

    Title: 입력받아 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int f, b;
+    cin >> f;
+    cin.get();
+    cin >> b;
+
+    cout << f << b;
+
+    return 0;
+}
