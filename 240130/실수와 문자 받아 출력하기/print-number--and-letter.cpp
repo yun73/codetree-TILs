e25eaff01399a5dd5e4e24d7634310f871f250a0
이윤형 <139411504+yun73@users.noreply.github.com>
2024-01-30 21:36:51 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char c;
+    double a,b;
+    cin >> c >> a >> b;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << c << endl;
+    cout << a << endl;
+    cout << b ;
+
+    return 0;
+}
