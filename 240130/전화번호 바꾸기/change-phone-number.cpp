c2053e2b56bfdf539d921bef0cbb2f373b67f329
이윤형 <139411504+yun73@users.noreply.github.com>
2024-01-30 23:19:49 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int s, m, e;
+
+    cin >> s;
+    cin.get();
+    cin >> m;
+    cin.get();
+    cin >> e;
+
+    cout << "010-" << e << "-" << m;
+
+    return 0;
+}
