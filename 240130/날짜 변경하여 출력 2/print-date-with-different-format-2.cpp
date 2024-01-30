2681744293092e2a65d4852341ef3c4e118757d2
이윤형 <139411504+yun73@users.noreply.github.com>
2024-01-30 23:10:47 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m,d,y;
+
+    cin >> m;
+    cin.get();
+    cin >> d;
+    cin.get();
+    cin >> y;
+
+    cout << y << "." << m << "." << d;
+
+
+    return 0;
+}
