e4a8136bef79e2ca5c55e6d0eefc1bff936bc6d9
이윤형 <139411504+yun73@users.noreply.github.com>
2024-01-30 21:25:26 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a,b,c;
+
+    cin >> a >> b >> c;
+
+    cout << fixed;
+
+    cout.precision(3);
+
+    cout << a << endl << b << endl << c;
+
+    return 0;
+}
