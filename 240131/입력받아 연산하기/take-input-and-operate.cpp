fcf93466f0d6f8bd073454e039f551780b25de6b
이윤형 <139411504+yun73@users.noreply.github.com>
2024-01-31 20:31:12 +0900

    Title: 입력받아 연산하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240131/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin >> a >> b;
+
+    a += 87;
+
+    b %= 10;
+
+    cout << a << endl;
+    cout << b << endl;
+
+    return 0;
+}
