94a7dc84d1e07fdfdc9f373d27a54df7d6882d5a
이윤형 <139411504+yun73@users.noreply.github.com>
2024-01-31 21:03:35 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240131/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c;
+    cin >> a >> b >> c;
+    
+    int sum = a+b+c;
+    int avg =  sum/3;
+
+    
+    cout << sum << endl;
+    cout << avg << endl;
+    cout << sum - avg ;
+
+
+    return 0;
+}
