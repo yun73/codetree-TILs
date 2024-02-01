0bb6e821f26a38303acf096497d6d5b329798f68
이윤형 <139411504+yun73@users.noreply.github.com>
2024-02-01 22:26:17 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240201/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int num ;
+    cin >> num;
+
+    if (num == 1) {
+        cout << "John";
+    } else if (num == 2) {
+        cout << "Tom";
+    } else if (num == 3) {
+        cout << "Paul";
+    } else {
+        cout << "Vacancy";
+    }
+
+    return 0;
+}
