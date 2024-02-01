5537f76ffa977447fe95c93940b316e29a246ebe
이윤형 <139411504+yun73@users.noreply.github.com>
2024-02-01 22:29:58 +0900

    Title: 등급 매기기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240201/등급 매기기/ranking.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    if (n >= 90) {
+        cout << "A";
+    } else if (n >= 80) {
+        cout << "B";
+    } else if (n >= 70) {
+        cout << "C";
+    } else if (n >= 60) {
+        cout << "D";
+    } else {
+        cout << "F";
+    } 
+
+
+    return 0;
+}
