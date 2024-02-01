9b7383ed4867c39aa1bccaf6a43a36ef1f359cc9
이윤형 <139411504+yun73@users.noreply.github.com>
2024-02-01 22:10:15 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240201/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double avg;
+    cin >> avg;
+
+    if (avg >= 1.0) {
+        cout << "High";
+    } else if ( avg >= 0.5) {
+        cout << "Middle";
+    } else {
+        cout << "Low";
+    }
+
+
+    return 0;
+}
