1431f6255e60760148b323a44dd57d120a1a789f
이윤형 <139411504+yun73@users.noreply.github.com>
2024-02-01 21:40:13 +0900

    Title: 숫자의 조건 여부, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240201/숫자의 조건 여부/number's-condition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a;
+    cin >> a;
+    if ( a >= 113) {
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+
+    return 0;
+}
