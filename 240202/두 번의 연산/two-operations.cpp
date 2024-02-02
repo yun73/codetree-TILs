40c5bda8211b5c61dcaacdc5adcff26cbffaca48
이윤형 <139411504+yun73@users.noreply.github.com>
2024-02-02 23:56:51 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240202/두 번의 연산/two-operations.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+
+    if (a%2==1) {
+        a += 3;
+    }
+
+    if (a%3 == 0) {
+        a /= 3;
+    }
+
+    cout << a;
+
+    return 0;
+}
