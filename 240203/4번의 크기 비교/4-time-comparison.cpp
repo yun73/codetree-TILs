4f766cf5fce951fd017a9650fd256a6504f0e2f8
이윤형 <139411504+yun73@users.noreply.github.com>
2024-02-03 20:47:59 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240203/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a,b,c,d,e;
+    cin >> a;
+    cin >> b >> c >> d >> e;
+
+    cout << (a > b) << endl;
+    cout << (a > c) << endl;
+    cout << (a > d) << endl;
+    cout << (a > e) << endl;
+
+    return 0;
+}
