a4c5454f0b3ac2187a1eec73df5c505c96259e11
이윤형 <139411504+yun73@users.noreply.github.com>
2024-02-03 23:27:56 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240203/윤년인가/is-leap-year.cpp
+++ 240203/윤년인가/is-leap-year.cpp
@@ -6,7 +6,7 @@ int main() {
     int y;
     cin >> y;
 
-    if ( y %4 != 0 || (y %4 == 0 && y % 100 ==0)) {
+    if ( y %4 != 0 || (y %4 == 0 && y % 100 ==0 && y % 400 !=0)) {
         cout << "false";
     } else {
         cout << "true";
