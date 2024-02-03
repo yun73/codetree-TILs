972518d769b011376a9df19d65c7e7a974f19a6b
이윤형 <139411504+yun73@users.noreply.github.com>
2024-02-03 21:17:30 +0900

    Title: 숫자의 조건 여부 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240203/숫자의 조건 여부 3/number's-condition-3.cpp
+++ 240203/숫자의 조건 여부 3/number's-condition-3.cpp
@@ -5,8 +5,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a;
     cin >> a;
-
-    if ( ~(a%13) || ~(a%19) ) {
+    if ( a%13==0 || a%19==0 ) {
         cout << "True";
     } else {
         cout << "False";
