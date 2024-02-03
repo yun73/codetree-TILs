f7ac87ef568e89a34be593f9d9e2c6ce6ad21a30
이윤형 <139411504+yun73@users.noreply.github.com>
2024-02-03 20:40:42 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240203/비교 연산/comparison-operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+
+    cin >> a >> b;
+
+    cout << (a >= b) << endl;
+    cout << (a > b) << endl;
+    cout << (a <= b) << endl;
+    cout << (a < b) << endl;
+    cout << (a == b) << endl;
+    cout << (a != b) << endl;
+
+    return 0;
+}
