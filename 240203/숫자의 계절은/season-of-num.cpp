0c1e9c7c32e9772cef79a1dda4bdd6600451f666
이윤형 <139411504+yun73@users.noreply.github.com>
2024-02-03 21:48:16 +0900

    Title: 숫자의 계절은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240203/숫자의 계절은/season-of-num.cpp
+++ 240203/숫자의 계절은/season-of-num.cpp
@@ -6,14 +6,14 @@ int main() {
     int m;
     cin >> m;
 
-    if ( m >=3 && m <=5) {
-        cout << "Spring";
-    } else if (  m >=6 && m <=8 ) {
-        cout << "Summer";
-    } else if ( m >=9 && m <=11) {
-        cout << "Fall";
-    } else {
+    if ( m >=12 && m <=2) {
         cout << "Winter";
+    } else if (  m <=5 ) {
+        cout << "Spring";
+    } else if ( m <=8) {
+        cout << "Summer";
+    } else {
+        cout << "Fall";
     }
 
     return 0;
