1e5c6543ebcdc65d53a81709c5b5a0d180a1a441
이윤형 <139411504+yun73@users.noreply.github.com>
2024-02-05 20:13:51 +0900

    Title: b부터 a까지 감소, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240205/b부터 a까지 감소/dec-from-b-to-a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin >> a >> b;
+
+    for (int i=b;i >= a ; i--) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
