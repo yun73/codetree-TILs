33d77b67453b0d3e08feaaba2349e60977fe89db
이윤형 <139411504+yun73@users.noreply.github.com>
2024-02-05 20:22:27 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240205/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    cin >> n;
+
+    while ( n > 0) {
+        cout << "*" << endl;
+        n--;
+    }
+
+    return 0;
+}
