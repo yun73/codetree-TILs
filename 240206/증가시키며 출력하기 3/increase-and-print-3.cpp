bf148e8f7baaaa46edce05340176911929ea522a
이윤형 <139411504+yun73@users.noreply.github.com>
2024-02-06 20:08:15 +0900

    Title: 증가시키며 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240206/증가시키며 출력하기 3/increase-and-print-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int i = 10;
+
+    while (i <= 26) {
+        cout << i << " ";
+        i += 2;
+    }
+    return 0;
+}
