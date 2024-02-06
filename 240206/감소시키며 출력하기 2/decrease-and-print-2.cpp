57977a18dad3c8954c34f8a85e98d7b2412c7dc7
이윤형 <139411504+yun73@users.noreply.github.com>
2024-02-06 20:14:43 +0900

    Title: 감소시키며 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240206/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int i =26;
+
+    while (i >= 10) {
+        cout << i << " ";
+        i--;
+    }
+
+    return 0;
+}
