db01a8e746b19a5bfb65e1a360685c9d2d570dc5
이윤형 <139411504+yun73@users.noreply.github.com>
2024-02-19 00:56:20 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240219/369 게임/369-game.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+
+
+        if (i % 3 == 0) {
+            cout << 0 << " ";
+            continue;
+        }
+
+        int j = i;
+        int cnt = 0;
+        while ( j > 0) {
+             //  현재 수를 10으로 나눈 나머지 == 현재 판별할 수
+            if (j % 10 == 3 || j % 10 == 6 ||  j % 10 == 9 ) {
+                cnt += 1;
+            }
+
+            j /= 10; // 현재 수를 10으로 나눈 몫 == 다음 수
+        }
+
+        if ( cnt > 0) {
+            cout << cnt << " ";
+        } else {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
