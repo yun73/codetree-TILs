3d4583e54a518e3077c0c705bf5926364d63a59c
이윤형 <139411504+yun73@users.noreply.github.com>
2024-03-04 23:59:12 +0900

    Title: 특정 조건 배수의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240304/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
+++ 240304/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -6,9 +6,9 @@ int main() {
     int a,b;
     cin >> a >> b;
     int sum = 0;
-    for (int i = a; i <=b ; i++){
+    for (int i = a; i <=b ;i++) {
         if (i%5==0) {
-            sum += i;
+        sum += i;
         }
     }
 
